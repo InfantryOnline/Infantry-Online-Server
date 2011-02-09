d5195b21d5402c16251c9214d894baeac9c8661a
HellSpawn <hellspawn>
2011-02-09 06:34:31 +0000

    Added DBServer to trunk/InfDatabase

---
+++ InfDatabase/DBComm/Packets/Player/CS_PlayerUpdate.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using InfServer.Network;
+using InfServer.Data;
+
+namespace InfServer.Protocol
+{	/// <summary>
+	/// CS_PlayerUpdate contains player statistical information for updating
+	/// </summary>
+	public class CS_PlayerUpdate<T> : PacketBase
+		where T : IClient
+	{	// Member Variables
+		///////////////////////////////////////////////////
+		public PlayerInstance player;		//The player instance we're referring to
+
+		public Data.PlayerStats stats;	
+
+		//Packet routing
+		public const ushort TypeID = 3;
+		static public event Action<CS_PlayerUpdate<T>, T> Handlers;
+
+
+		///////////////////////////////////////////////////
+		// Member Functions
+		//////////////////////////////////////////////////
+		/// <summary>
+		/// Creates an empty packet of the specified type. This is used
+		/// for constructing new packets for sending.
+		/// </summary>
+		public CS_PlayerUpdate()
+			: base(TypeID)
+		{
+			player = new PlayerInstance();
+		}
+
+		/// <summary>
+		/// Creates an instance of the dummy packet used to debug communication or 
+		/// to represent unknown packets.
+		/// </summary>
+		/// <param name="typeID">The type of the received packet.</param>
+		/// <param name="buffer">The received data.</param>
+		public CS_PlayerUpdate(ushort typeID, byte[] buffer, int index, int count)
+			: base(typeID, buffer, index, count)
+		{
+			player = new PlayerInstance();
+		}
+
+		/// <summary>
+		/// Routes a new packet to various relevant handlers
+		/// </summary>
+		public override void Route()
+		{	//Call all handlers!
+			if (Handlers != null)
+				Handlers(this, (_client as Client<T>)._obj);
+		}
+
+		/// <summary>
+		/// Serializes the data stored in the packet class into a byte array ready for sending.
+		/// </summary>
+		public override void Serialize()
+		{	//Type ID
+			Write((byte)TypeID);
+
+			Write(player.id);
+			Write(player.magic);
+
+			stats.Serialize(this);
+		}
+
+		/// <summary>
+		/// Deserializes the data present in the packet contents into data fields in the class.
+		/// </summary>
+		public override void Deserialize()
+		{
+			player.id = _contentReader.ReadUInt16();
+			player.magic = _contentReader.ReadInt32();
+
+			stats = PlayerStats.Deserialize(_contentReader);
+		}
+
+		/// <summary>
+		/// Returns a meaningful of the packet's data
+		/// </summary>
+		public override string Dump
+		{
+			get
+			{
+				return "Player stat update";
+			}
+		}
+	}
+}
