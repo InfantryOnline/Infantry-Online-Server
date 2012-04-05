15077868c723198aca9ccedfdf156298a648cdb0
HellSpawn <hellspawn>
2012-04-05 10:00:59 +0000

    Forgot some files from awhile ago, sorry fowl :p

---
+++ bin/scripts/GameTypes/Frontlines/TDM.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+using InfServer.Logic;
+using InfServer.Game;
+using InfServer.Scripting;
+using InfServer.Bots;
+using InfServer.Protocol;
+
+using Assets;
+
+namespace InfServer.Script.GameType_FL_TDM
+{	// Script Class
+    /// Provides the interface between the script and arena
+    ///////////////////////////////////////////////////////
+    class Script_FL_TDM : Scripts.IScript
+    {	///////////////////////////////////////////////////
+        // Member Variables
+        ///////////////////////////////////////////////////
+        private Arena _arena;					//Pointer to our arena class
+        private CfgInfo _config;				//The zone config
+
+        private Team _victoryTeam;				//The team currently winning!
+        private int _tickGameLastTickerUpdate;
+        private int _lastGameCheck;				//The tick at which we last checked for game viability
+        private int _tickGameStarting;			//The tick at which the game began starting (0 == not initiated)
+        private int _tickGameStart;				//The tick at which the game started (0 == stopped)
+        //Settings
+        private int _minPlayers;				//The minimum amount of players
+        private List<Vehicle> _supplyBags;
+
+
+        ///////////////////////////////////////////////////
+        // Member Functions
+        ///////////////////////////////////////////////////
+        /// <summary>
+        /// Performs script initialization
+        /// </summary>
+        public bool init(IEventObject invoker)
+        {	//Populate our variables
+            _arena = invoker as Arena;
+            _config = _arena._server._zoneConfig;
+
+            _minPlayers = 2;
+            _supplyBags = new List<Vehicle>();
+
+            return true;
+        }
+
+        /// <summary>
+        /// Allows the script to maintain itself
+        /// </summary>
+        public bool poll()
+        {	//Should we check game state yet?
+            int now = Environment.TickCount;
+
+            if (now - _lastGameCheck <= Arena.gameCheckInterval)
+                return true;
+            _lastGameCheck = now;
+
+            //Do we have enough players ingame?
+            int playing = _arena.PlayerCount;
+
+            if ((_tickGameStart == 0 || _tickGameStarting == 0) && playing < _minPlayers)
+            {	//Stop the game!
+                _arena.setTicker(1, 1, 0, "Not Enough Players");
+                _arena.gameReset();
+            }
+
+            //Update our tickers
+            if (_tickGameStart > 0 && now - _arena._tickGameStarted > 2000)
+            {
+                if (now - _tickGameLastTickerUpdate > 5000)
+                {
+                    updateTickers();
+                    _tickGameLastTickerUpdate = now;
+                }
+            }
+
+            //Do we have enough players to start a game?
+            else if (_tickGameStart == 0 && _tickGameStarting == 0 && playing >= _minPlayers)
+            {	//Great! Get going
+                _tickGameStarting = now;
+                _arena.setTicker(1, 1, _config.deathMatch.startDelay * 100, "Next game: ",
+                    delegate()
+                    {	//Trigger the game start
+                        _arena.gameStart();
+                    }
+                );
+            }
+            return true;
+        }
+
+        #region Events
+
+
+        /// <summary>
+        /// Called when a player enters the game
+        /// </summary>
+        [Scripts.Event("Player.Enter")]
+        public void playerEnter(Player player)
+        {
+        }
+
+        /// <summary>
+        /// Called when a player leaves the game
+        /// </summary>
+        [Scripts.Event("Player.Leave")]
+        public void playerLeave(Player player)
+        {
+        }
+
+        /// <summary>
+        /// Called when the game begins
+        /// </summary>
+        [Scripts.Event("Game.Start")]
+        public bool gameStart()
+        {	//We've started!
+            _tickGameStart = Environment.TickCount;
+            _tickGameStarting = 0;
+
+
+            //Let everyone know
+            _arena.sendArenaMessage("Game has started!", 1);
+            _arena.setTicker(1, 1, _config.deathMatch.timer * 100, "Time Left: ",
+            delegate()
+            {	//Trigger game end.
+                _arena.gameEnd();
+            }
+            );
+
+            foreach (Team t in _arena.Teams)
+            {
+                t._calculatedKills = 0;
+                t._calculatedDeaths = 0;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Updates our tickers
+        /// </summary>
+        public void updateTickers()
+        {
+            string format;
+            if (_arena.ActiveTeams.Count() > 1)
+            {
+                format = String.Format("{0}={1} - {2}={3}",
+                    _arena.ActiveTeams.ElementAt(0)._name,
+                    _arena.ActiveTeams.ElementAt(0)._calculatedKills,
+                    _arena.ActiveTeams.ElementAt(1)._name,
+                    _arena.ActiveTeams.ElementAt(1)._calculatedKills);
+                _arena.setTicker(1, 0, 0, format);
+            }
+
+
+        }
+
+
+        /// <summary>
+        /// Triggered when a player notifies the server of an explosion
+        /// </summary>
+        [Scripts.Event("Player.Explosion")]
+        public bool playerExplosion(Player player, ItemInfo.Projectile weapon, short posX, short posY, short posZ)
+        {	//Dog Tag?
+            if (weapon.id == 1072)
+            {
+                    player.Experience += 500;
+                    player.syncState();
+                    player.sendMessage(0, "Dogtag reward: 500xp");
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Called when the game ends
+        /// </summary>
+        [Scripts.Event("Game.End")]
+        public bool gameEnd()
+        {	//Game finished, perhaps start a new one
+
+            _arena.sendArenaMessage("Game Over");
+
+            _arena.breakdown(false);
+
+
+            foreach (Player p in _arena.PlayersIngame)
+            {
+                //No rewards for him!
+                if (p.StatsLastGame.kills == 0 && p.StatsLastGame.deaths == 0)
+                    continue;
+
+                int cash = 3000 + (100 * p.StatsLastGame.kills);
+                int experience = 1700 + (50 * p.StatsLastGame.kills);
+                int points = 500 + (100 * p.StatsLastGame.kills);
+                p.Cash += cash;
+                p.KillPoints += points;
+                p.Experience += experience;
+
+                p.sendMessage(0, String.Format("Personal Award: (Cash={0}) (Experience={1}) (Points={2})", cash, experience, points));
+                p.resetVars();
+                p.syncState();
+            }
+
+            _tickGameStart = 0;
+            _tickGameStarting = 0;
+            _victoryTeam = null;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Called when the statistical breakdown is displayed
+        /// </summary>
+        [Scripts.Event("Game.Breakdown")]
+        public bool breakdown()
+        {	//Allows additional "custom" breakdown information
+
+
+            //Always return true;
+            return true;
+        }
+
+        /// <summary>
+        /// Called to reset the game state
+        /// </summary>
+        [Scripts.Event("Game.Reset")]
+        public bool gameReset()
+        {	//Game reset, perhaps start a new one
+            _tickGameStart = 0;
+            _tickGameStarting = 0;
+
+            _victoryTeam = null;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Handles the spawn of a player
+        /// </summary>
+        [Scripts.Event("Player.Spawn")]
+        public bool playerSpawn(Player player, bool bDeath)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Triggered when a player wants to unspec and join the game
+        /// </summary>
+        [Scripts.Event("Player.JoinGame")]
+        public bool playerJoinGame(Player player)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Triggered when a player wants to spec and leave the game
+        /// </summary>
+        [Scripts.Event("Player.LeaveGame")]
+        public bool playerLeaveGame(Player player)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Triggered when a player has died, by any means
+        /// </summary>
+        /// <remarks>killer may be null if it wasn't a player kill</remarks>
+        [Scripts.Event("Player.Death")]
+        public bool playerDeath(Player victim, Player killer, Helpers.KillType killType, CS_VehicleDeath update)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Triggered when one player has killed another
+        /// </summary>
+        [Scripts.Event("Player.PlayerKill")]
+        public bool playerPlayerKill(Player victim, Player killer)
+        {
+            victim._team._calculatedDeaths++;
+            killer._team._calculatedKills++;
+            return true;
+        }
+        #endregion
+    }
+}
