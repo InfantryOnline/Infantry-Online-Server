597d9711bd9d4a0f365de08c732314505cac6112
x15inf <x15inf>
2014-04-11 06:47:40 +0000

    Updating auth command to give sysop
    -Mizz

--- InfServer/Game/Commands/Mod/Basic.cs
+++ InfServer/Game/Commands/Mod/Basic.cs
@@ -130,7 +130,7 @@ namespace InfServer.Game.Commands.Mod
             {
                 if (payload.Trim() == player._server._config["server/managerPassword"].Value)
                 {
-                    player._permissionTemp = Data.PlayerPermission.ArenaMod;
+                    player._permissionTemp = Data.PlayerPermission.Sysop;
                     player.sendMessage(1, "You have been granted");
                 }
             }
