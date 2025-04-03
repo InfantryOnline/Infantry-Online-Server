471fda3bb5954fb6ce3c8c9d734f1d9123c925f8
Axidus <qwerty5544@aim.com>
2025-04-03 08:00:49 -0700

    Update GamePlay.cs
    using System.Net.WebClient

--- scripts/GameTypes/USL/Events/GamePlay.cs
+++ scripts/GameTypes/USL/Events/GamePlay.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Net
+using System.Net.WebClient
 
 using InfServer.Logic;
 using InfServer.Game;
