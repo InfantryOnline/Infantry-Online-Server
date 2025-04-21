208b2d137ae095d661327d3b769c2867b741f628
Jovan Stanojlovic <contact@jovan-s.com>
2025-04-21 11:53:43 -0400

    Updated recycler path.

--- dotnetcore/Server/Program.cs
+++ dotnetcore/Server/Program.cs
@@ -33,7 +33,7 @@ namespace InfServer
             }
             else
             {
-                Process.Start(Environment.CurrentDirectory + $"{Path.DirectorySeparatorChar}Server.exe");
+                Process.Start(Environment.CurrentDirectory + $"{Path.DirectorySeparatorChar}ZoneServer.exe");
                 Stop();
             }
         }
