e4075e1e804264e778f662992f81dd06d72bfdff
Jovan <contact@jovan-s.com>
2026-03-14 02:38:16 -0400

    Log packet details for undefined packet type

--- dotnetcore/DirectoryServer/Directory/Protocol/Factory.cs
+++ dotnetcore/DirectoryServer/Directory/Protocol/Factory.cs
@@ -31,6 +31,7 @@ namespace InfServer.DirectoryServer.Directory.Protocol
                     //Undefined packet type
                     //TODO: Type 7
                     packet = new PacketDummy(typeID, buffer, index, count);
+                    Console.WriteLine($"{0}: {1}", packet.Dump, packet.DataDump);
                     break;
             }
 
